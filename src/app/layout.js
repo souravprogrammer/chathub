15fa0be1aaf910bbecb049da0999a455740064f7
sourav sharma <souravsh1234567@gmail.com>
2024-02-07 15:04:14 +0530

    added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,10 @@ export const metadata = {
 };
 export const viewport = {
   themeColor: "#020817",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 export default function RootLayout({ children }) {
   return (
