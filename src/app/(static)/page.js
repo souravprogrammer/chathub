35a2c7abe5e387dbeec24edeb851a3a96105a107
sourav sharma <souravsh1234567@gmail.com>
2024-02-17 16:54:56 +0530

    seo h1 added

--- src/app/(static)/page.js
+++ src/app/(static)/page.js
@@ -31,10 +31,10 @@ export default function Home() {
           <div className="container relative flex-1">
             <article className="left-0  absolute flex flex-row justify-center flex-1 py-10 gap-x-10 align-center w-[100%] h-[100%]">
               <div className="flex flex-col justify-center text-center gap-y-7 align-center">
-                <h4 className={"text-5xl font-bold leading-relaxed "}>
+                <h1 className={"text-5xl font-bold leading-relaxed "}>
                   Talk to strangers,
                   <br /> Make Friends!
-                </h4>
+                </h1>
 
                 <p className="max-w-[900px]">
                   Discover an interesting free random chat alternative to make
